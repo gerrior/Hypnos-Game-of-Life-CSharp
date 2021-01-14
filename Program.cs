0ad4ceb85e0e8a319c982b48289d7c30939bd228
Mark Gerrior <github@gerrior.com>
2021-01-14 13:36:47 -0800

    Base applicatin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hypnos_Game_of_Life_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
